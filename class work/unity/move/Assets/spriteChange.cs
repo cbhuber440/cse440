bd43a877a889a5c1a7e19ef62f3653ebae53ce32
Christopher Huber <004134532@coyote.csusb.edu>
2014-10-23 22:09:59 -0700

    Completed input and movement assignment

---
+++ class work/unity/move/Assets/spriteChange.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spriteChange : MonoBehaviour {
+	public Sprite sprite1;
+	public Sprite sprite2;
+	private SpriteRenderer sr;
+
+	// Use this for initialization
+	void Start () {
+		sr = gameObject.GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			if (sr.sprite == sprite1) {
+				sr.sprite = sprite2;
+			} else {
+				sr.sprite = sprite1;
+			}
+		}
+	}
+}
